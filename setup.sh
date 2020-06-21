410a69e5449b51077265df52c7d0689fdfa527fb
Karl Arthur <karl.crispen.arthur@gmail.com>
2020-06-21 17:56:52 -0600

    feat: Have setup script retrieve GitHub submodules

--- setup.sh
+++ setup.sh
@@ -3,13 +3,15 @@
 # Load the defined variables
 . vars.sh
 
+# Don't allow non-root user
 if [ "${USER}" == "root" ]; then
   echo -e "Run this script using a non-root user with \"sudo\" privileges!"
   exit 1
 fi
 
+# Set up Java
 if [ -d "${DIR_JAVA}/bin" ]; then
-  echo -e "\"${DIR_JAVA}\" already exists!\nUnnecessary to set up again!"
+  echo -e "\"${DIR_JAVA}\" already exists!\nUnnecessary to set up Java again!"
 else
   echo -e "Installing Java at \"${DIR_JAVA}\""
   # If not present then download java.tar.gz
@@ -35,5 +37,27 @@ else
   else
     echo -e "Something went wrong and we do not have \"${DIR_DOWNLOADS}/${FILE_JAVA}\" to install Java with"
     exit 1
+  fi
+fi
+
+# Retrieve GitHub submodules
+if [ -a ".gitmodules" ]; then
+  DIR_MAIN_SUBMODULE="$( cat .gitmodules | grep "^\s\+path\s=" | sed "s|\\s\+path\\s=\\s||g" )"
+  if [ ! -f "${DIR_MAIN_SUBMODULE}/README.md" ]; then
+    echo -e "Initializing the main GitHub submodule (${DIR_MAIN_SUBMODULE})"
+    git submodule update --init
+  else
+    echo -e "Unnecessary to initialize main GitHub submodule (${DIR_MAIN_SUBMODULE}) again!"
+  fi
+  if [ -a "${DIR_MAIN_SUBMODULE}/.gitmodules" ]; then
+    DIR_SUB_SUBMODULE="$( cat ${DIR_MAIN_SUBMODULE}/.gitmodules | grep "^\s\+path\s=" | sed "s|\\s\+path\\s=\\s||g" )"
+    if [ ! -d "${DIR_MAIN_SUBMODULE}/${DIR_SUB_SUBMODULE}/eng" ]; then
+      echo -e "Initializing the GitHub sub-sub-module (${DIR_MAIN_SUBMODULE}/${DIR_SUB_SUBMODULE})"
+      pushd nukkit > /dev/null
+      git submodule update --init
+      popd > /dev/null
+    else
+      echo -e "Unnecessary to re-initialize sub-sub-module (${DIR_MAIN_SUBMODULE}/${DIR_SUB_SUBMODULE}) again!"
+    fi
   fi
 fi
